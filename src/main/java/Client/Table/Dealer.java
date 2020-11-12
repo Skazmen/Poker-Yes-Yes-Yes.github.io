0637543b59a2bfc58ee07744ca472c4efd231fae
Skazmen <skaziuwielkisz69@gmail.com>
2020-11-12 14:11:06 +0100

    add functions at Dealer

--- src/main/java/Client/Table/Dealer.java
+++ src/main/java/Client/Table/Dealer.java
@@ -5,7 +5,17 @@ import Client.Deck.Deck;
 import java.util.Collections;
 
 public class Dealer {
+    private final Deck deck;
+
+    public Dealer() {
+        this.deck = new Deck();
+    }
+
     public void shuffleDeck(Deck deck) {
         Collections.shuffle(deck);
+    }
+
+    public Deck getDeck() {
+        return deck;
     }
 }
