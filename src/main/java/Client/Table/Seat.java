55b59729c9970fb09b8f0a4aca53a948b2c94471
Rivus44 <rivus44@gmail.com>
2020-11-16 17:12:55 +0100

    added seats

---
+++ src/main/java/Client/Table/Seat.java
@@ -0,0 +1,16 @@
+package Client.Table;
+
+import Client.Players.Player;
+
+public class Seat {
+    public int value;
+    public Player player;
+
+    public Seat(int value, Player player) {
+
+        this.value = value;
+        this.player = player;
+    }
+
+
+}
